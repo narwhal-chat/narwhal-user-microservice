2d9a3fe0c18147a88a8f644a295339139bcef354
sleex89 <samlee723@gmail.com>
2018-04-15 13:54:47 -0700

    Refactored queries and getting multiple errors

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const promise = require('bluebird');
 const utils = require('../utility.js');
 const options = {
-    promiseLib: promise,
+	promiseLib: promise,
 };
 const bcrypt = require('bcrypt');
 
@@ -9,166 +9,154 @@ const pgp = require('pg-promise')(options);
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/narwhal_users';
 const db = pgp(connectionString);
 
-// Add query functions
-function createUser(req, res, next) {
-  const user = {
-    username: req.username,
-    password: req.password,
-    email_address: req.email_address
-  };
+const user = {
+	createUser: async (req, res, next) => {
+        const user = {
+            username: req.username,
+            password: req.password,
+            email_address: req.email_address,
+        };
 
-  // Define all of the default user avatars
-  const defaultUserAvatars = [
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+1.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+2.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+4.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+3.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+5.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+6.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+7.svg',
-    'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+8.svg'
-  ];
-  // Randomly choose an avatar
-  user.avatar = defaultUserAvatars[Math.floor(Math.random() * defaultUserAvatars.length)];
+        // Define all of the default user avatars
+        const defaultUserAvatars = [
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+1.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+2.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+4.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+3.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+5.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+6.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+7.svg',
+            'https://s3-us-west-1.amazonaws.com/narwhalavatar/user-default-avatars/User+Avatar+8.svg',
+        ];
+        // Randomly choose an avatar
+        user.avatar = defaultUserAvatars[Math.floor(Math.random() * defaultUserAvatars.length)];
 
-  db.none(
-      'insert into users(username, password, email_address, avatar)' +
-      'values(${username}, ${password}, ${email_address}, ${avatar})',
-    user
-  )
-    .then(() => {
-      const token = utils.generateToken(user);
-      db.one("SELECT * FROM users WHERE username = ${username}", {
-        username: user.username,
-      })
-        .then(data => {
-          res.json({
-              token: token,
-              user: data,
-          });
-        });
-      })
-      .catch(function(err) {
-        console.log('Error In Backend', err)
-        let detail = err.constraint;
-        let message = '';
-        if (detail === 'user_ak_username') {
-            message = 'Username already exists';
-        } else if (detail === 'user_ak_email_address') {
-            message = 'E-mail already ef xists';
-        }
-        return res.status(404).json({
-            error: true,
-            message: message
-        });
-      });
-}
-
-function loginUser(req, res, next) {
-    db.one("SELECT * FROM users WHERE username = ${username}", {
-            username: req.username
-    })
-    .then(user => {
-        bcrypt.compare(req.password, user.password, (err, valid) => {
-            if (!valid) {
+        db.none(
+                'insert into users(username, password, email_address, avatar)' +
+                    'values(${username}, ${password}, ${email_address}, ${avatar})',
+                user
+            )
+            .then(() => {
+                const token = utils.generateToken(user);
+                db
+                    .one('SELECT * FROM users WHERE username = ${username}', {
+                        username: user.username,
+                    })
+                    .then(data => {
+                        res.json({
+                            token: token,
+                            user: data,
+                        });
+                    });
+            })
+            .catch(function(err) {
+                console.log('Error In Backend', err);
+                let detail = err.constraint;
+                let message = '';
+                if (detail === 'user_ak_username') {
+                    message = 'Username already exists';
+                } else if (detail === 'user_ak_email_address') {
+                    message = 'E-mail already exists';
+                }
                 return res.status(404).json({
                     error: true,
-                    message: 'Username or Password is Wrong',
+                    message: message,
                 });
-            }
-            let userData = {
-                id: user.id,
-                username: user.username,
-                email_address: user.email_address,
-                avatar: user.avatar,
-                create_date: user.create_date
-            }
-            console.log('userdata', userData);
+            });
+    },
+    loginUser: async (req, res, next) => {
+        db.one('SELECT * FROM users WHERE username = ${username}', {
+            username: req.username,
+        })
+        .then(user => {
+            bcrypt.compare(req.password, user.password, (err, valid) => {
+                if (!valid) {
+                    return res.status(404).json({
+                        error: true,
+                        message: 'Username or Password is Wrong',
+                    });
+                }
+                let userData = {
+                    id: user.id,
+                    username: user.username,
+                    email_address: user.email_address,
+                    avatar: user.avatar,
+                    create_date: user.create_date,
+                };
+                console.log('userdata', userData);
 
-            let token = utils.generateToken(userData);
+                let token = utils.generateToken(userData);
 
-            res.json({
-                user: userData,
-                token: token
+                res.json({
+                    user: userData,
+                    token: token,
+                });
+            });
+        })
+        .catch(error => {
+            return res.status(404).json({
+                error: true,
+                message: 'Username does not exist',
             });
         });
-    })
-    .catch(error => {
-        return res.status(404).json({
-            error: true,
-            message: 'Username does not exist'
-        })
-    });
-}
+    },
 
-function editProfile  (req, res, next) {
-    console.log('req.body', req);
-    db.one("SELECT * FROM users WHERE username = ${username}", {
-        username: req.username
-    })
-    .then(user => {
-        bcrypt.compare(req.password, user.password, (err, valid) => {
+    editProfile: async (req, res, next) => {
+        let errors = {
+            error: false,
+            errorType: {
+                password: '',
+                username: '',
+                email: ''
+            }
+        }
+        const currentUser = await db.one('SELECT * FROM users WHERE username = ${username}', { username: req.username })
+        const checkUser = await bcrypt.compare(req.password, currentUser.password, (err, valid) => {
+
             if (!valid) {
-                console.log('error 3');
-                return res.status(401).json({
-                    error: true,
-                    message: 'Password is incorrect',
-                    errorType: 'password'
-                })
-            }
-            let newUsername = req.newUsername;
-            let email = req.email;
-            let avatar = req.avatar;
-			if (newUsername === '') {
-				newUsername = user.username;
-			}
-			if (email === '') {
-				email = user.email_address;
-            }
-            if (avatar === '') {
-                avatar = user.avatar;
+              errors.error = true;
+              errors.errorType.password = 'Password is incorrect'
+              return res.status(401).json(errors)
             }
 
-			db.any(`UPDATE users SET username = '${newUsername}', email_address = '${email}', avatar = '${avatar}' WHERE username = '${req.username}'`)
-				.then(() => {
-					db.one(`SELECT * FROM users WHERE username = '${newUsername}'`)
-						.then(user => {
-                            console.log('yay', user)
-                            let userData = { id: user.id, username: user.username, email_address: user.email_address, avatar: user.avatar, create_date: user.create_date };
-                            let token = utils.generateToken(userData);
+        });
 
-                            res.json({ user: userData, token: token });
-                        })
-                        .catch(err => {
-                            console.log('error getting user data');
-                        });
-                })
-                .catch(err => {
-                    console.log('error 2');
-                    let detail = err.constraint;
-                    let message = '';
-                    let errorType = '';
-                    if (detail === 'user_ak_email_address') {
-                        message = 'E-mail already exists';
-                        errorType = 'email';
-                        
-                    }
-                    if (detail === 'user_ak_username') {
-                        message = 'Username already exists';
-                        errorType = 'username';
-                    }
+        let newUsername = req.newUsername;
+        let email = req.email;
+        let avatar = req.avatar;
 
-                    return res.status(401).json({ error: true, message: message, errorType: errorType});
-                });
-        })
-    })
-    .catch (err => {
-        console.log(err)
-    })
-}
+        if (newUsername === '') {
+            newUsername = currentUser.username;
+        }
+        if (email === '') {
+            email = currentUser.email_address;
+        }
+
+        const updateUsername = await db.any('UPDATE users SET username = ${newUsername}, avatar = ${avatar} WHERE username = ${username}', 
+            { newUsername, avatar, username: req.username})
+            .catch(e => {
+                errors.error = true;
+                errors.errorType.username = 'Username already exists';
+            })
+        const updateEmail = await db.any('UPDATE users SET email_address = ${email} where USERNAME = ${username}',
+            { email, avatar, username: req.username })
+            .catch(e => {
+                errors.error = true;
+                errors.errorType.email = 'Email already exists';
+            })
+            
+        if (errors.error) {
+            return res.json(401).json(errors)
+        } else {
+            const updatedUser = await db.one('SELECT * FROM users WHERE username = ${newUsername}', { newUsername })
+            const token = await utils.generateToken(updatedUser);
+            res.json({ user: updatedUser, token})
+        }
+    }
+
+};
+
 
 module.exports = {
-    createUser: createUser,
-    loginUser: loginUser,
-    editProfile: editProfile
+    user: user
 };
--- server.js
+++ server.js
@@ -14,13 +14,6 @@ const PORT = process.env.PORT || 3033;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// // Set Static Path
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// } else {
-// 	app.use(express.static(__dirname + '/../client/build'));
-// }
-
 app.post('/register', (req, res, next) => {
 	var body = req.body;
 	console.log('body in register post', body);
@@ -31,21 +24,20 @@ app.post('/register', (req, res, next) => {
 		password: hash,
 		email_address: body.email_address,
 		avatar: body.avatar,
-		create_date: body.create_date
+		create_date: body.create_date,
 	};
 
-	db.createUser(user, res, next);
+	db.user.createUser(user, res, next);
 });
 
 app.post('/login', (req, res, next) => {
-	db.loginUser({ username: req.body.username, password: req.body.password }, res, next);
+	db.user.loginUser({ username: req.body.username, password: req.body.password }, res, next);
 });
 
 app.post('/editProfile', (req, res, next) => {
 	console.log('edit profile in microservice', req.body);
-	db.editProfile(req.body, res, next) 
-
-})
+	db.user.editProfile(req.body, res, next);
+});
 
 // Listening to port
 app.listen(PORT);
