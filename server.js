4ab63c7bcdb3ca25f36e5843077e15ee340ea2b8
Rory Well <rory.e.well@gmail.com>
2018-04-15 23:45:14 -0500

    Remove old comments and code

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const PORT = process.env.PORT || 3033;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Route to register a user
 app.post('/register', (req, res, next) => {
-	var body = req.body;
-	console.log('body in register post', body);
+	const body = req.body;
 
-	var hash = bcrypt.hashSync(body.password.trim(), 10);
-	var user = {
+	const hash = bcrypt.hashSync(body.password.trim(), 10);
+	const user = {
 		username: body.username,
 		password: hash,
 		email_address: body.email_address,
@@ -30,10 +30,12 @@ app.post('/register', (req, res, next) => {
 	db.user.createUser(user, res, next);
 });
 
+// Route to log in
 app.post('/login', (req, res, next) => {
 	db.user.loginUser({ username: req.body.username, password: req.body.password }, res, next);
 });
 
+// Route to edit a profile
 app.post('/editProfile', (req, res, next) => {
 	console.log('edit profile in microservice', req.body);
 	db.user.editProfile(req.body, res, next);
